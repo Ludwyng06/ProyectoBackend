b06c27d9b9b66ea239a9d4f0c9b5817cebdafd57
Jenser <ludwynghf@gmail.com>
2024-09-04 20:37:30 -0500

    Carga-02

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
 const http = require('http');
 
 const hostname = '127.0.0.1';
+const fs=require("fs")
 const port = 3000;
+
+//
+const  PaginaAyuda =fs.readFileSync("ayuda.html")
+const  PaginaContacto =fs.readFileSync("contacto.html")
+const  PaginaInicio =fs.readFileSync("inicio.html")
+const  PaginaError =fs.readFileSync("paginaNoEncontrada.html")
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
+
+  if (req.url === "/Ayuda") {
+    return res.end(PaginaAyuda);
+  } else if (req.url === "/Contacto") {
+    return res.end(PaginaContacto);
+  } else if (req.url === "/") {
+    return res.end(PaginaInicio );
+  } else {
+    res.writeHead(404);
+    res.end(PaginaError);
+  }
 });
 
 server.listen(port, hostname, () => {
